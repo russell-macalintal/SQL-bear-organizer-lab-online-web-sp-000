d5383454974659d6aff4d6f5075112b40bffcc9f
Russell Macalintal <russell.macalintal@gmail.com>
2019-12-18 05:03:30 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,17 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Mr. Chocolate", 3, "M", "Black", "Hot", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Rowdy", 1, "M", "White", "Rough", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Tabitha", 10, "F", "Gray", "Sweet", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Sergeant Brown", 5, "M", "Blue", "Stoic", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Melissa", 3, "F", "Pink", "Friendly", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Grinch", 100, "M", "Green", "Grouchy", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES ("Wendy", 50, "F", "Black", "Sassy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+  VALUES (NULL, 7, "F", "Purple", "Wild", 0);
+    
